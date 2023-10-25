f9d0c2070022104188a3ca1529580edbf693e047
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-25 10:04:09 +0000

    Updating to latest

--- lib/onboarding/onboarding_model.dart
+++ lib/onboarding/onboarding_model.dart
@@ -1,13 +1,13 @@
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'home_page_widget.dart' show HomePageWidget;
+import 'onboarding_widget.dart' show OnboardingWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+class OnboardingModel extends FlutterFlowModel<OnboardingWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
